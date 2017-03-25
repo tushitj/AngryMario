5ce9770932e775de253748655fa06e5d10b64371
Tushit Jain <tushitjain@Tushits-MacBook-Air.local>
2017-03-24 22:36:41 -0400

    ball motion within the frame. goes left and right on wall contact

--- project5/project5/GameScene.swift
+++ project5/project5/GameScene.swift
@@ -11,15 +11,15 @@ import GameplayKit
 import CoreMotion
 
 //var hero : Hero!
-var left : SKSpriteNode!
-var right : SKSpriteNode!
-var top : SKSpriteNode!
-var bottom : SKSpriteNode!
-var hero : SKSpriteNode!
-var ball : SKShapeNode!
-
-var leftCategory : UInt32 = 0x1 << 0
-var ballCategory : UInt32 = 0x1 << 1
+var left : SKSpriteNode = SKSpriteNode()
+var right : SKSpriteNode = SKSpriteNode()
+var top : SKSpriteNode = SKSpriteNode()
+var bottom : SKSpriteNode = SKSpriteNode()
+var hero : SKSpriteNode = SKSpriteNode()
+var ball : SKShapeNode = SKShapeNode()
+var moveball = 1;
+var leftCategory : UInt32 = 0
+var ballCategory : UInt32 = 0b1
 
 
 var val = 0
@@ -64,6 +64,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact) {
      //   gets called automatically when two objects begin the contact
+    
+//        var bodyA : SKPhysicsBody!
+//        var bodyB : SKPhysicsBody!
+//        bodyA = contact.bodyA
+//        bodyB = contact.bodyB
+        
+//        if(contact.bodyA.categoryBitMask > contact.bodyB.categoryBitMask){
+//            bodyB = contact.bodyA
+//            bodyA = contact.bodyB
+//        }
+//        if (bodyA.categoryBitMask & leftCategory) != 0 && (bodyB.categoryBitMask & ballCategory != 0) {
+//            
+//            print("Collison")
+//        }
+//        
+    
     }
     
     func didEnd(_ contact: SKPhysicsContact) {
@@ -80,10 +96,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.fillColor = UIColor.red
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ballRad)
         ball.physicsBody?.isDynamic = true;
-        ball.physicsBody?.usesPreciseCollisionDetection = true
-        ball.physicsBody?.categoryBitMask = ballCategory
-        ball.physicsBody?.collisionBitMask = leftCategory | ballCategory
-        ball.physicsBody?.contactTestBitMask = leftCategory | ballCategory
+        //ball.physicsBody?.usesPreciseCollisionDetection = true
+       // ball.physicsBody?.categoryBitMask = ballCategory
+        //ball.physicsBody?.collisionBitMask = 0
+       // ball.physicsBody?.contactTestBitMask = leftCategory
         
         
         ball.physicsBody?.restitution = 0.7
@@ -100,8 +116,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         left = SKSpriteNode(color: wallColor, size: CGSize(width: wallWidth, height: self.frame.size.height))
         left.physicsBody = SKPhysicsBody(rectangleOf: left.size)
         left.physicsBody?.isDynamic = false;
-        left.physicsBody?.usesPreciseCollisionDetection = true
-        left.physicsBody?.categoryBitMask = leftCategory
+        //left.physicsBody?.usesPreciseCollisionDetection = true
+        //left.physicsBody?.categoryBitMask = leftCategory
+        //left.physicsBody?.contactTestBitMask = ballCategory
+        //left.physicsBody?.collisionBitMask = 0
         left.position = CGPoint(x: -self.frame.size.width/2, y: 0)
         addChild(left)
         
@@ -127,11 +145,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func jumpBall(){
         ball.physicsBody?.velocity = CGVector(dx: 0, dy: 10)
-        ball.physicsBody!.applyImpulse(CGVector(dx:3, dy:20))
+        if(moveball == 0){
+            ball.physicsBody!.applyImpulse(CGVector(dx:3, dy:20))
+        }
+        else if(moveball == 1){
+            ball.physicsBody!.applyImpulse(CGVector(dx:-3, dy:20))
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         jumpBall()
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        if(ball.position.x < -self.frame.size.width/2+25){
+            moveball = 0;
+        }
+        else if(ball.position.x > self.frame.size.width/2-25){
+            moveball = 1;
+        }
     }
     
     
